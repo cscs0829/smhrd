5a4838ebda47748b4181d120ccb5d0823543d1d1
cscs0829 <cscs0829@naver.com>
2025-09-25 10:22:36 +0900

    asd

--- src/components/features/ImageLinkGenerator.tsx
+++ src/components/features/ImageLinkGenerator.tsx
@@ -739,7 +739,7 @@ export function ImageLinkGenerator() {
                       <TableHeader className="sticky top-0 bg-background z-10">
                         <TableRow>
                           <TableHead className="w-12">순번</TableHead>
-                          <TableHead className="w-[300px]">링크</TableHead>
+                          <TableHead className="w-[500px]">링크</TableHead>
                           <TableHead className="w-16">상태</TableHead>
                           <TableHead className="w-16">작업</TableHead>
                         </TableRow>
